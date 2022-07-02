72775f6266ac9b396ecb6504c31e8b2d6ac0aa20
AnonymousUser002 <105080005+AnonymousUser002@users.noreply.github.com>
2022-07-02 22:07:48 +1000

    Update tflite2pytorch.py

--- tflite2pytorch.py
+++ tflite2pytorch.py
@@ -1,8 +1,10 @@
 import os
 import argparse
 import torch
+import numpy as np
 import onnx
 import onnxruntime as ort
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from onnx_pytorch.onnx_pytorch import ConvertModel
 from modifier.pruning import pruning
@@ -12,7 +14,7 @@ from utils.utils import *
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_name', type=str, help='path of tflite model', default='mobilenet_v1_1.0_224_quant')
+parser.add_argument('--model_name', type=str, help='path of tflite model', default='63_human_256_input_1_output_0')
 parser.add_argument('--alpha', type=int, default=0.0, help='threshold for auto-matching')
 parser.add_argument('--save_onnx', type=bool, default=False)
 opt = parser.parse_args()
@@ -22,7 +24,7 @@ def onnx_modifier(onnx_model):
     pruning(onnx_model)
     translation(onnx_model)
     auto_matching(onnx_model, similarity=opt.alpha)
-tflite_model_path = './tflite_model/'
+tflite_model_path = '/home/mzho0045/rm46_scratch/mingyi/model_convert/dl/'
 model_path = tflite_model_path + opt.model_name + '.tflite'
 inputs = generate_random_data(model_path)
 tflite_out, output_details = test_tflite_results(model_path, inputs)
@@ -33,6 +35,7 @@ onnx_modifier(onnx_model)
 onnx.save(onnx_model, './out_model/'+opt.model_name+'_modified.onnx')
 onnx.checker.check_model(onnx_model)
 pytorch_model = ConvertModel(onnx_model).eval()
+torch.save(pytorch_model, './out_model/'+opt.model_name+'_modified.pt')
 output_pt = pytorch_model(torch.from_numpy(inputs[0]))
 error = np.absolute(output_pt[output_details[0]['name']].detach().squeeze().to(torch.float).numpy()-tflite_out.squeeze())
 print('final_error mean:', error.mean())
